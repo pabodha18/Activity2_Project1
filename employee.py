1a76cc911ccf4d3938483963f60e73a3fff42136
pabodha <pabodha.madhawee@gmail.com>
2021-10-08 14:09:22 +0530

    Create python file

---
+++ employee.py
@@ -0,0 +1,18 @@
+class Employee:
+    company = 'Tutorial Gateway'
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def func_message(self):
+        print(self.name + ' is learning Python Programming')
+
+
+emp1 = Employee('Mike Anderson', 25, 'Male')
+print(emp1.company)
+print(emp1.name)  # Mike
+print(emp1.age)  # 25
+print(emp1.gender)
+emp1.func_message()
